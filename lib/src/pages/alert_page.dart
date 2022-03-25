754eca61d5289b3bbf61d06744f917495b092467
jlmetrion <84740844+jlmetrion@users.noreply.github.com>
2022-03-25 00:45:40 -0400

    master

--- lib/src/pages/alert_page.dart
+++ lib/src/pages/alert_page.dart
@@ -9,6 +9,14 @@ class AlertPage extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Alert Page'),
       ),
+      body: Center(
+        child: ElevatedButton(
+          child: const Text('Mostrar Alerta'),
+          onPressed: () => _mostrarAlert(context),
+          style: ElevatedButton.styleFrom(
+              primary: Colors.red, shape: const StadiumBorder()),
+        ),
+      ),
       floatingActionButton: FloatingActionButton(
         child: const Icon(Icons.arrow_back),
         onPressed: () {
@@ -17,4 +25,30 @@ class AlertPage extends StatelessWidget {
       ),
     );
   }
+}
+
+void _mostrarAlert(BuildContext context) {
+  showDialog(
+      context: context,
+      barrierDismissible: true,
+      builder: (context) {
+        return AlertDialog(
+          shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20.0)) ,
+          title: const Text('Titulo'),
+          content: Column(mainAxisSize: MainAxisSize.min, children: const [
+            Text('Este es el contenido de la caja de la Alerta'),
+            FlutterLogo(size: 100.0)
+          ]),
+          actions: [
+            TextButton(
+                child: const Text('Cancelar'),
+                onPressed: () => Navigator.of(context).pop()),
+            TextButton(
+                child: const Text('OK'),
+                onPressed: () {
+                  Navigator.of(context).pop();
+                })
+          ],
+        );
+      });
 }
